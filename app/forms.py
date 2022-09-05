55ef46c47d2b596e1bfa758352162b9a01f1c520
sangkips <sangkipkoech@gmail.com>
2022-09-05 20:59:21 +0300

    added a better UI to frontend

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
 
 from app.models import Todo
+from django import forms
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
-        fields = ['title', 'description']
+        fields = ['title', 'description', 'completed']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Product description'}),
+        }
+    
